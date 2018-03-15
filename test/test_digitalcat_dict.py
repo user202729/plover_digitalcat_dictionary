fc3618870e37b0d1e49d88a613975575e4a070bb
Thomas Thurman <thomas@thurman.org.uk>
2018-03-15 19:27:19 +0000

    some basic tests

---
+++ test/test_digitalcat_dict.py
@@ -0,0 +1,33 @@
+# Copyright (c) 2017 Thomas Thurman
+# See LICENSE.txt for details.
+
+import unittest
+
+EXPECTED = {
+   ('TKPWHREPB',): 'glen',
+   ('A',): 'a',
+   ('TK*EB', 'TK*EB', 'R-R'):
+        # no, I don't know why; it was in the sample dct
+        '"Debbie \'Do Anything for Hillary\' Wasserman Schultz"',
+   ('TKEUGZ', 'AER'):
+        'dictionary',
+   ('HAOER',): 'here',
+   ('SEZ',): 'says',
+   ('S',): 'is',
+   ('SKW-FRPBLGTS',): '{^{A}And}',
+   ('STKPWHRAEPBG',): '{^{Q}What, if anything,^}',
+   ('SPHAUL',): 'small',
+}
+
+from plover_digitalcat_dictionary import DigitalCATDictionary
+
+class TestCase(unittest.TestCase):
+
+    def test_load_dictionary(self):
+
+        # this test can produce useful output if it fails
+        self.maxDiff = None
+
+        filename = 'test/digitalcat-test.dct'
+        d = DigitalCATDictionary.load(filename)
+        self.assertEqual(dict(d.items()), EXPECTED)
